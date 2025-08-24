91b4c0b59990d77b51382e207b45601ef49d4330
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-08-24 16:56:38 -0300

    Add Pedra, Papel ou tesoura

---
+++ Exercicios-CSharp/src/Intermediários/JogoDaVelha/JogoDaVelha/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine ("Pedra, Papel ou Tesoura?");
+
+Console.WriteLine ("Escolha uma opção: ");
+string userInput = (Console.ReadLine() ??"").ToLower();
+
+string[] opcoes = {"pedra", "papel", "tesoura"};
+Random rnd = new Random();
+string escolhaComputador = opcoes[rnd.Next(opcoes.Length)];
+
+
+if (userInput == escolhaComputador) { 
+    Console.WriteLine($"Empate! Ambos escolheram {userInput}.");
+}
+else if ((userInput == "pedra" && escolhaComputador == "tesoura") ||
+         (userInput == "papel" && escolhaComputador == "pedra") ||
+         (userInput == "tesoura" && escolhaComputador == "papel")) {
+    Console.WriteLine($"Você venceu! {userInput} vence {escolhaComputador}.");
+}
+else if ((userInput == "pedra" && escolhaComputador == "papel") ||
+         (userInput == "papel" && escolhaComputador == "tesoura") ||
+         (userInput == "tesoura" && escolhaComputador == "pedra")) {
+    Console.WriteLine($"Você perdeu! {escolhaComputador} vence {userInput}.");
+}
+else {
+    Console.WriteLine("Opção inválida. Por favor, escolha pedra, papel ou tesoura.");
+}
